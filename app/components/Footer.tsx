cbf761999bb07557c10287d91c0cd88ccdbf2957
YourName <your-email@example.com>
2026-01-17 09:05:33 +0100

    footer resooponsive

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,16 +1,19 @@
 'use client';
-import React, { useEffect, useState } from 'react';
-
 const Footer = () => {
-
   return (
-    <>
-   <footer className="bg-slate-900 text-slate-400 text-center py-6 text-sm">
+    <footer
+      className="
+        bg-slate-900 text-slate-400 
+        text-center 
+        py-4 sm:py-6 
+        text-xs sm:text-sm md:text-base
+        px-2
+      "
+    >
+      <p className="max-w-md mx-auto leading-relaxed">
         © {new Date().getFullYear()} Kayode Ayomide Damilare · All Rights Reserved
-      </footer>
-
-     
-    </>
+      </p>
+    </footer>
   );
 };
 
